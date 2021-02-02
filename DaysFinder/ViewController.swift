970f57bce4ed29e841486180f4bb285f1ffac8ba
rihardslozins <rihards.lozins@gmail.com>
2021-02-02 12:40:49 +0200

    Button Radius Changed

--- DaysFinder/ViewController.swift
+++ DaysFinder/ViewController.swift
@@ -22,7 +22,6 @@ class ViewController: UIViewController {
     
     @IBAction func findWeekDayTapped(_ sender: Any) {
         
-        
         let calendar = Calendar.current
         
         var dateComponents = DateComponents()
